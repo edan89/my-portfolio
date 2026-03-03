2fd6f3f09fdc3c1dc25166c8bd134706bea5a681
edan89 <edanviera@hotmail.com>
2026-03-03 21:18:55 +0200

    chore: remove temporary fix scripts

--- fix_encoding.ps1
+++
@@ -1,4 +0,0 @@
-$content = [System.IO.File]::ReadAllText("index.html")
-$utf8 = New-Object System.Text.UTF8Encoding($true)
-[System.IO.File]::WriteAllText("index.html", $content, $utf8)
-Write-Host "Encoding fixed to UTF-8 with BOM"
